822da4f78f8d378de71b4816a6177c8bfc5f3879
mknepper <jake.m.knepper@gmail.com>
2019-08-04 18:55:30 -0500

    Added new code for variables.

--- variables/main.py
+++ variables/main.py
@@ -4,10 +4,10 @@ c = a + b # The variable 'c' is now equal to 3 or 'a' + 'b'.
 # Calculate 'c' to be 'a' + 'b', or 3. The value will stay stored in this variable.
 print(c)
 
-a = 2 # We will change the value of 'a' to 2. Variables can be changed in Python, otherwise known as mutable. 
+a = 2 # We will change the value of 'a' to 2. Variables can be changed in Python, otherwise known as mutable.
 b = 2
 c = a + c # This now becomes 3 + 2, because we have calculated 'c' to be 3.
-# The value of 'c' is still 3 from earlier, now result in 5 because we've added 'a' or 2.
+# The value of 'c' is still 3 from earlier, now resulting in 5 because we've added 'a' or 2.
 print(c)
 
 a = 2
