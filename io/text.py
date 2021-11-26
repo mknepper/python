9a661353026ffedfc26ddbe9325180cbf5365f8e
mknepper <jake.m.knepper@gmail.com>
2021-11-26 16:24:08 -0600

    Added

--- io/text.py
+++ io/text.py
@@ -1,4 +1,7 @@
-import os
 import sys
-
+print("In this script, we're going to print some text to a file.")
+with open('io.txt', 'w') as f:
+    f.write("You wrote...\n")
+    f.write(input("Enter text here: "))
+    f.close()
 sys.exit()
